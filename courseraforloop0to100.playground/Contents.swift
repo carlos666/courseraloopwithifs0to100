303422f45b949becc64441cac21b773832143932
carlos666 <deadstein>
2015-12-14 01:02:12 -0600

    Commit de prueba Desde SourceTree

---
+++ courseraforloop0to100.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - : a place where people can play
+
+import UIKit
+
+for var i=0; i<=100; i++ {
+    
+    if(i % 5 == 0){
+        print("\t \(i) Bingo \n ")
+    }
+    
+    if(i % 2 == 0){
+        print("\t \(i) par \n")
+    }
+    
+    if(i % 2 != 0){
+        print("\t \(i) impar \n")
+    }
+    
+    if(i >= 30 && i <= 40 ){
+        print(" \t \(i) Viva Swift")
+    }
+}
